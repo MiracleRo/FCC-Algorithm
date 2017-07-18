6a5a2407e5405302b1727b3f0beb2c12b48b4772
MiracleRo <741556866@qq.com>
2017-07-18 21:49:03 +0800

    add Finders Keepers.js  Smallest Common Multiple.js(not competle)

---
+++ Smallest Common Multiple.js
@@ -0,0 +1,13 @@
+function smallestCommons(arr) {
+	var bigNum = arr[0]>arr[1]?arr[0]:arr[1];
+	var smallNum = arr[0]>arr[1]?arr[1]:arr[0];
+	var result =1;
+	for(i=smallNum;i<=bigNum;i++){
+		
+	}
+	console.log(result);
+  	return result;
+}
+
+
+smallestCommons([5,1]);
